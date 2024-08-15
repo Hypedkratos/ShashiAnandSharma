a57971b2ed10230491a198174b62ac1116d4b627
Hypedkratos <shashianand8651@gmail.com>
2024-08-15 21:31:37 +0530

    added new resume

--- components/downloadRes/downloadRes.jsx
+++ components/downloadRes/downloadRes.jsx
@@ -1,15 +1,17 @@
 const DownloadResumeButton = () => {
-    return (
-        <div><a
-            href="#"
-            download="/resume/Shashi Anand Sharma(Resume).pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transform transition duration-200 hover:scale-105"
-        >
-            Download Resume
-        </a></div>
-    );
+  return (
+    <div>
+      <a
+        href="#"
+        download="/resume/ShashiResume.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transform transition duration-200 hover:scale-105"
+      >
+        Download Resume
+      </a>
+    </div>
+  );
 };
 
 export default DownloadResumeButton;

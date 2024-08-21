7c2ec27410255ebdf06be1adb936f109f33c1f5b
Hypedkratos <shashianand8651@gmail.com>
2024-08-21 12:59:03 +0530

    new resume added

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/navbar/navbar";
 
 const about = () => {
   const downloadResume = () => {
-    window.location.href = "/resume/ShashiResume.pdf";
+    window.location.href = "/resume/Shashi-Anand-Sharma(Resume).pdf";
   };
 
   return (
